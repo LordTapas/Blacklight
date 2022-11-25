f1caa5c6546bb456d8e2d43539ee3edbd14c7789
LordTapas <107563859+LordTapas@users.noreply.github.com>
2022-11-25 17:58:25 -0500

    Delete bldjs.js

--- bldjs.js
+++
@@ -1,8 +0,0 @@
-$("[draggable-item]").draggable({ scroll: true, scrollSpeed: 100, scrollSensitivity: 100, handle: "[draggable-handle]" });
-
-/* CLOCK */
-let clock = document.getElementById('timestamp');
-setInterval(function () {
-	let timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" });
-	clock.innerText = timestamp;
-  }, 1000);
